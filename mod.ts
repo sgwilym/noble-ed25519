7af9329476ff2f2a0e524a9f78e36d09704efc63
Sam Gwilym <gwilym@me.com>
2022-01-25 11:16:34 +0000

    Use webcrypto instead of external sha512 lib

--- mod.ts
+++ mod.ts
@@ -1,8 +1,9 @@
-import { getPublicKey, sign, verify, utils, CURVE, Point, ExtendedPoint } from './index.ts';
-import { Sha512 } from 'https://deno.land/std@0.119.0/hash/sha512.ts';
-
-utils.sha512 = async (message: Uint8Array): Promise<Uint8Array> => {
-  return new Uint8Array(new Sha512().update(message).arrayBuffer());
-};
-
-export { getPublicKey, sign, verify, utils, CURVE, Point, ExtendedPoint };
+export {
+  CURVE,
+  ExtendedPoint,
+  getPublicKey,
+  Point,
+  sign,
+  utils,
+  verify,
+} from "./index.ts";
